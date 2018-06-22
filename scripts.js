d1289c2d83fad3784007e0f54b2b8d992671271b
jywkim <jkim589@gmail.com>
2018-06-21 23:52:03 -0400

    add RGB split filter
    pull apart the different red, green, and blue and move to either side

--- scripts.js
+++ scripts.js
@@ -25,7 +25,8 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -47,6 +48,15 @@ function redEffect(pixels) {
     pixels.data[i] = pixels.data[i + 0] + 200;
     pixels.data[i + 1] = pixels.data[i + 1] - 50;
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 500] = pixels.data[i + 1];
+    pixels.data[i - 550] = pixels.data[i + 2];
   }
   return pixels;
 }
